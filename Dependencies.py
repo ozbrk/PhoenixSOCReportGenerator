a77fced332c3da5cda6c29a2ea24e566a1c2ad30
ozberk <ozberkakbas1@outlook.com>
2021-05-12 00:17:39 +0300

    New Installer Here

--- Dependencies.py
+++ Dependencies.py
@@ -1,47 +1,47 @@
-# -*- coding: utf-8 -*-
-import subprocess
-import sys
+# # -*- coding: utf-8 -*-
+# import subprocess
+# import sys
 
-print("Dependencies:")
-print(" - Requests")
-print(" - Termcolor")
+# print("Dependencies:")
+# print(" - Requests")
+# print(" - Termcolor")
 
-answer = input("Dependencies will be installed. Do you want to proceed? [Yes/No]:")
+# answer = input("Dependencies will be installed. Do you want to proceed? [Yes/No]:")
 
 
-if answer == "Yes":
-        os = input("What is your operating system? [Windows / Linux]")
-        if os == "Windows":
-                try:
-                        subprocess.call('powershell.exe pip3 install requests', shell=True)
-                except:
-                        raise
-                        sys.exit(0)
-                try:
-                        subprocess.call('powershell.exe pip3 install termcolor', shell=True)
-                except:
-                        raise
-                        sys.exit(0)
+# if answer == "Yes":
+#         os = input("What is your operating system? [Windows / Linux]")
+#         if os == "Windows":
+#                 try:
+#                         subprocess.call('powershell.exe pip3 install requests', shell=True)
+#                 except:
+#                         raise
+#                         sys.exit(0)
+#                 try:
+#                         subprocess.call('powershell.exe pip3 install termcolor', shell=True)
+#                 except:
+#                         raise
+#                         sys.exit(0)
 		
-                print("İnstallation complete. You may use the script at ease.")
+#                 print("İnstallation complete. You may use the script at ease.")
 
-        if os == "Linux":
-                try:
-                        subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-                except:
-                        raise
-                        sys.exit(0)
-                try:
-                        subprocess.check_call([sys.executable, "-m", "pip", "install", "termcolor"])
-                except:
-                        raise
-                        sys.exit(0)
+#         if os == "Linux":
+#                 try:
+#                         subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+#                 except:
+#                         raise
+#                         sys.exit(0)
+#                 try:
+#                         subprocess.check_call([sys.executable, "-m", "pip", "install", "termcolor"])
+#                 except:
+#                         raise
+#                         sys.exit(0)
 		
                 
-                print("İnstallation complete. You may use the script at ease.") 
+#                 print("İnstallation complete. You may use the script at ease.") 
                         
-else: 
-	print("Bye")
-	SystemExit
+# else: 
+# 	print("Bye")
+# 	SystemExit
 
-k= input("Press Enter to exit") 
+# k= input("Press Enter to exit") 
---
+++ setup.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import subprocess
+import json
+
+print("Welcome to the Phoneix SOC Reporting and Quick Analysing Tool")
+print("Ozberk Akbas - 2021")
+
+print("Setup will have 3 stepps. In the first step you need to add your api keys as they are asked.")
+print("Next part of the setup process will be dependencies installation. There are roughly three major ones")
+print(" - Requests") 
+print(" - Termcolor")
+
+print("""You may also have questions about the project so type "Help" to the following question. """)
+
+answer = str(input("Shall We Continue?[Yes/No/Help]:"))
+
+if answer == "Yes":
+
+    authkeyexch = str(input("XForce Exchange API Key:"))
+    authpasswdexch = str(input("XForce Exchange API Password:"))
+    vtapikey = str(input("Virus Toal API Key:"))
+    abuseapikey = str(input("AbuseIP DB API Key:"))
+
+    cred = {}
+    cred["authkeyexch"] = authkeyexch
+    cred["aauthpasswdexch"] = authpasswdexch
+    cred["vtapikey"] = vtapikey
+    cred["abuseapikey"] = abuseapikey
+
+    with open("apicreds.json", "w") as credfile:
+        json.dump(cred, credfile)
+
+    try:
+        os.system('uname -a')
+
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "termcolor"])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+    
+    except:
+
+        subprocess.call('powershell.exe pip3 install requests', shell=True)
+        subprocess.call('powershell.exe pip3 install termcolor', shell=True)
+
+    print("Installation Complete")
+    x = input("Press Enter to Exit")
+
+if answer == "No":
+    x = input("Press Enter to Exit")
+    sys.exit(0)
+
+if answer == "Help":
+    print("----------------------------------------------------")
+    print("Requirements:")
+    print("Termcolor: Termcolor is a python library for printing colored outputs on console screen like bash or powershell.")
+    print("Requests: Requests is a python library for making http requests and processing the responses as codes or content.")
+    print("""The program will ask you for your apikeys to use them for authorizating with the api's. It also store them inside a file called "apicreds.json". You can change them from there. """)
+    print("Thank you for visiting this lonely help page.")
+    print("----------------------------------------------------")
+    sys.exit(0)
